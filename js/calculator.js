60a73e246970fb987f2c1d67847e0dae89e82fb8
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-07 14:19:48 +0300

    Add functions to subtract, divide and multiply

--- js/calculator.js
+++ js/calculator.js
@@ -9,5 +9,59 @@ var number1 = parseInt(prompt("Enter a number:"));
 /*Ask user for number2*/
 var number2 = parseInt(prompt("Enter a another number:"));
 
+/*Add 2 numbers*/
+var resultAdd = add(number1, number2);
+
 /*Display results*/
-alert(add(number1, number2));
+alert(resultAdd);
+
+/*Function to subtract 2 numbers*/
+var subtract = function(number1, number2) {
+  return number1 - number2
+};
+
+/*Ask user for number1*/
+var number1 = parseInt(prompt("Enter a number:"));
+
+/*Ask user for number2*/
+var number2 = parseInt(prompt("Enter a another number:"));
+
+/*Subtract 2 numbers*/
+var resultSubtract = subtract(number1, number2);
+
+/*Display results*/
+alert(resultSubtract);
+
+/*Function to multiply 2 numbers*/
+var multiply = function(number1, number2) {
+  return number1 * number2
+};
+
+/*Ask user for number1*/
+var number1 = parseInt(prompt("Enter a number:"));
+
+/*Ask user for number2*/
+var number2 = parseInt(prompt("Enter a another number:"));
+
+/*Multiply 2 numbers*/
+var resultMultiply = multiply(number1, number2);
+
+/*Display results*/
+alert(resultMultiply);
+
+/*Function to divide 2 numbers*/
+var divide = function(number1, number2) {
+  return number1 / number2
+};
+
+/*Ask user for number1*/
+var number1 = parseInt(prompt("Enter a number:"));
+
+/*Ask user for number2*/
+var number2 = parseInt(prompt("Enter a another number:"));
+
+/*Multiply 2 numbers*/
+var resultDivide = divide(number1, number2);
+
+/*Display results*/
+alert(resultDivide);
